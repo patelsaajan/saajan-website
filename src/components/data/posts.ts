7cfe5842769cefafb07978a44f275677616d0293
Saajan Patel <saajanpatel@hotmail.com>
2023-10-05 10:59:56 +0800

    recent post title edits

--- src/components/data/posts.ts
+++ src/components/data/posts.ts
@@ -1,18 +1,18 @@
 const postDataArray = [
   {
-    title: "Making a design system from scratch",
+    title: "Alison Becker v David de Gea",
     date: "12/02/2022",
     tags: "Design, Patten",
     body: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
   },
   {
-    title: "Creating pixel perfect icons in Figma",
+    title: "Liverpool revamped midfield analyst",
     date: "12/02/2022",
     tags: "Figma, Icon Design",
     body: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
   },
   {
-    title: "Making a design system from scratch",
+    title: "Klopps Liverpool throughout the years",
     date: "12/02/2022",
     tags: "Design, Patten",
     body: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
